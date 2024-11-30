74035cc2e7bc760153fd97f05ec321f8a4f7c00c
Bojan <99049054+BoyanStoychevski@users.noreply.github.com>
2024-11-30 11:47:45 +0100

    homework-1-js

---
+++ js-homework/js-homework-1/script.js
@@ -0,0 +1,45 @@
+let feetMeterProportion;
+feetMeterProportion = 0.3048;
+
+let distanceA;
+distanceA = 300;
+
+console.log(feetMeterProportion * distanceA);
+// ----------------------------------------------------------------------
+
+let sideA
+let sideB
+let areaRectangle
+
+sideA = 10;
+sideB = 30;
+areaRectangle = sideA * sideB
+
+console.log(areaRectangle);
+//---------------------------------------------------------------------------
+
+const pi = 3.14
+let r2
+let areaCircle
+
+r2 = 25
+areaCircle = pi * r2
+
+console.log(areaCircle)
+//-----------------------------------------------------------------------
+
+let phonePrice
+let phoneTax
+let phoneQuantity
+let phonePriceWithTax
+
+phonePrice = 119.95;
+phoneTax = 5 / 100;
+phoneQuantity = 30;
+phonePriceWithTax = (phonePrice * phoneTax) + phonePrice;
+
+console.log(phonePriceWithTax)
+console.log(phonePriceWithTax * phoneQuantity)
+// --------------------------------------------------------------------------
+
+
