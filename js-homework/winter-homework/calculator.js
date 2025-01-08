faeb7983c1cfaa6749b64ef3615f7fed80c2b719
Bojan <99049054+BoyanStoychevski@users.noreply.github.com>
2025-01-08 12:01:34 +0100

    winter-homework

---
+++ js-homework/winter-homework/calculator.js
@@ -0,0 +1,60 @@
+let currentNumber = '';
+let previousNumber = '';
+let operation = null;
+let calcInput = document.getElementById('display');
+
+function appendNumber(number) {
+    currentNumber = currentNumber + number;
+    calcInput.value = currentNumber;
+}
+
+function setOperation(op) {
+    if (currentNumber === '') {
+        return;
+    }
+    if (previousNumber !== '') {
+        calculate();
+    }
+    operation = op;
+    console.log("currentNumber", currentNumber);
+    console.log("previousNumber", currentNumber);
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+
+function calculate() {
+    if (operation === null || currentNumber === '' || previousNumber === '') {
+        return;
+    }
+    const prev = Number(previousNumber);
+    const curr = Number(currentNumber);
+    let result;
+
+    if (operation === '+') {
+        result = prev + curr;
+    } else if (operation === '-') {
+        result = prev - curr;
+    } else if (operation === '*') {
+        result = prev * curr;
+    } else if (operation === '/') {
+        result = prev / curr;
+    } else {
+        return;
+    }
+
+
+    currentNumber = result.toString();
+    operation = null;
+    previousNumber = '';
+    calcInput.value = currentNumber;
+}
+
+function clearDisplay() {
+    currentNumber = '';
+    previousNumber = '';
+    operation = null;
+    calcInput.value = currentNumber;
+}
+
+
+
